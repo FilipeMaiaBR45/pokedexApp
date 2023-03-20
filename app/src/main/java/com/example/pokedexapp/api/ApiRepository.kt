069d56b0fbcea7ee14a46a3ee22022f524f4e07d
FilipeMaiaBR45 <58606133+FilipeMaiaBR45@users.noreply.github.com>
2023-03-20 17:51:59 -0300

    ok

--- app/src/main/java/com/example/pokedexapp/api/ApiRepository.kt
+++ app/src/main/java/com/example/pokedexapp/api/ApiRepository.kt
@@ -1,14 +1,9 @@
 package com.example.pokedexapp.api
 
-import com.example.pokedexapp.api.pokemon.PokemonResponse
-import dagger.hilt.android.scopes.ActivityScoped
-import dagger.hilt.android.scopes.ServiceScoped
-import dagger.hilt.android.scopes.ViewModelScoped
 import retrofit2.Call
 import javax.inject.Inject
 
 //@ActivityScoped
-@ViewModelScoped
 class ApiRepository @Inject constructor(
    private val apiService: ApiService
 ) {
